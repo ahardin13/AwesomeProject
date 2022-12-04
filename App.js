ee407fe603ed336af12d91e56d3838291518c7bf
Andy Hardin <12464376+ahardin13@users.noreply.github.com>
2022-12-04 17:11:08 -0500

    Added player 1 money input

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
+import { Button, Form } from 'react-bootstrap';
 import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [player1Money, setPlayer1Money] = useState(0);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container} >
+      <div className='flex' >
+        <Form.Label>Player 1</Form.Label>
+        <Form.Control type="text" id="player1MoneyInput" value={player1Money} onChange={(event) => setPlayer1Money(parseInt(event.target.value))} />
+        <Button onClick={() => setPlayer1Money(player1Money + 1)}>Money Me!</Button>
+      </div>
+      {`Player 1 has \$${player1Money}`}
+      < StatusBar style="auto" />
     </View>
   );
 }
